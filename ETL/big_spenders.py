be72c0e990ad0ce58a3c18c6d42f08990de94e3b
Ioannis Sakkaris <johnsakk@Ioanniss-Air.fios-router.home>
2017-01-20 01:44:57 -0500

    add ETL folder with first analytic

---
+++ ETL/big_spenders.py
@@ -0,0 +1,30 @@
+import psycopg2
+import collections
+from collections import defaultdict
+import operator
+
+con=psycopg2.connect(dbname= 'spiceboysredshift', host='spice-boys-redshift-cluster.cp1oqvdsoh97.us-east-1.redshift.amazonaws.com', 
+port= '5439', user= 'spiceboy', password= 'SpiceBoys3')
+
+cur = con.cursor()
+
+cur.execute("""SELECT p_product_id, p_price from product""")
+
+product_prices = cur.fetchall()
+prod_dict = defaultdict(int)
+for row in product_prices:
+    print type(row[0])
+    prod_dict[row[0]] = row[1]
+
+cur.execute("""select orders.o_user_id, order_details.od_product_id, order_details.od_quantity from order_details join orders on o_order_id = od_order_id""")
+
+upq = cur.fetchall()
+
+aaa = defaultdict(int)
+
+for row in upq:
+    aaa[row[0]] += row[2]*prod_dict[row[1]]
+
+newaaa = dict(sorted(aaa.iteritems(), key=operator.itemgetter(1), reverse=True)[:10])
+
+print newaaa
